cb24895b50ed9ff537025e5e005d0dfa35a5e993
saqib-airflip <saqib@airflip.com>
2024-03-06 19:17:43 +0500

    add redirect component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,24 @@
 import '@/styles/globals.css'
-import { ClerkProvider } from '@clerk/nextjs'
+import { ClerkProvider, RedirectToSignIn, SignedIn, SignedOut } from '@clerk/nextjs'
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ClerkProvider {...pageProps}>
-      <Component {...pageProps} />
+      <SignedIn>
+        <Component {...pageProps} />
+      </SignedIn>
+      <SignedOut>
+        <RedirectToSignIn />
+      </SignedOut>
     </ClerkProvider>
   )
+
+  // return (
+  //   <ClerkProvider>
+  //     <Component {...pageProps} />
+  //   </ClerkProvider>
+  // )
 }
 
 export default MyApp;
